bf2caf3dddeb55b8cdca4b34b049fae4da244aca
Gegardus <stgegard@gmail.com>
2022-04-08 15:49:22 +0400

    Fix linter errors

--- my_enume.rb
+++ my_enume.rb
@@ -1,18 +1,18 @@
 module MyEnumerable
 
-  def all?(&block)
-    each { |item| return false unless yield item }
-      true
-    end
-  
-  def any?(&block)
-    each { |item| return true if yield item }
-      false
-    end
-  
-  def filter?(&block)
-    array = []
-    each { |item| array << item if yield item }
-    array
-    end   
+def all?(&block)
+  each { |item| return false unless yield item }
+    true
   end
+
+def any?(&block)
+  each { |item| return true if yield item }
+    false
+  end
+
+def filter?(&block)
+  array = []
+  each { |item| array << item if yield item }
+  array
+  end   
+end
